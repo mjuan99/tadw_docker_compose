d75385e2ffdba87d23abec97a64a3414f37b7144
Andrés Aguilar <andresaguilar1023@gmail.com>
2023-12-05 21:56:18 -0300

    movies microservice working with inline css

--- movies/index.js
+++ movies/index.js
@@ -3,25 +3,32 @@ const axios = require('axios');
 const app = express();
 const PORT = 3001;
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.static('movies:3001/styles'));
+
+
 app.get('/', async (req, res) => {
-    let random_status;
+    
+    let random_movies;
     try {
-        const random_response = await axios.get("http://random:3002/");
+        const random_response = await axios.get("http://random:3002/randommoviesinfo");
         if (!random_response.statusText == "OK") {
           throw new Error(`HTTP error! Status: ${random_response.status}`);
         }
-        random_status = random_response.data;
+        random_movies = random_response.data;
     } catch (error) {
         console.log(error);
         random_status = "Error";
     }
 
-    res.send({
+    res.render('index', {
         microservice: 'movies',
         dependencies: {
-            'random': random_status
+            'random': random_movies
         }
     });
+
 });
 
 app.listen(PORT, () => {
